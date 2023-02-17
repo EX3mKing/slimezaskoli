1b7615aaf463e058226aae406c51da5e294383a3
Borna Bošnjak <bornabosnjak2006@gmail.com>
2023-02-17 18:06:18 +0100

    Better Movement
    Jump buffering i coyote time

--- slimz/Assets/Skripte/PlayerMovement.cs
+++ slimz/Assets/Skripte/PlayerMovement.cs
@@ -12,11 +12,9 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField] private float speed_base_floor = 7f;
     [SerializeField] private float jump_strength_base = 13f;
-    [SerializeField] private float coyote_time = 0.2f;
-    [SerializeField] private float jump_buffer_time = 0.2f;
-    [SerializeField] private float jump_ienumerator_callback_time = 0.4f;
-    [Range(0f,1f)] [Tooltip("MORA BITI ISPOD 1 DA SE SMANJUJE")]
-    [SerializeField] private float jump_falloff = 0.5f;
+    [SerializeField] private float gravity_base;
+    [SerializeField] private float gravity_jumping;
+    [SerializeField] private float coyote_time;
 
     [Header("Collider References")] 
     [SerializeField] private LayerMask ground_layer;
@@ -28,14 +26,10 @@ public class PlayerMovement : MonoBehaviour
     private PlayerInputActions input_map;
     private InputAction movement;
     private Vector2 input_move_direction;
+    private float coyote_time_current;
     
     private bool hitting_base;
     private bool hitting_floor;
-
-    private float coyote_time_current;
-    private float jump_buffer_time_current;
-    private bool is_jumping;
-
 
     private void Awake()
     {
@@ -72,57 +66,41 @@ public class PlayerMovement : MonoBehaviour
     {
         hitting_base = ColliderCheck(collider_base, ground_layer);
         hitting_floor = ColliderCheck(collider_floor, ground_layer);
-        
-        /*
-        // coyote time and jump buffer
+
         if (hitting_base && hitting_floor)
         {
             coyote_time_current = coyote_time;
         }
         else
         {
-            if(coyote_time_current > 0f) coyote_time_current -= Time.deltaTime;
-        }
-        
-        if (input_move_direction.y > 0f)
-        {
-            jump_buffer_time_current = jump_buffer_time;
-        }
-        else
-        {
-            if (jump_buffer_time_current > 0f) jump_buffer_time_current -= Time.deltaTime;
+            if (coyote_time_current > 0f) coyote_time_current -= Time.fixedDeltaTime;
         }
 
-        if (coyote_time_current > 0f && jump_buffer_time_current > 0f && !is_jumping)
-        {
-            player_rb.velocity = new Vector2(player_rb.velocity.x, jump_strength_base);
-            jump_buffer_time_current = 0f;
-            StartCoroutine(JumpCooldown());
-        }
-
-        if (input_move_direction.y > 0f && player_rb.velocity.y > 0f)
-        {
-            player_rb.velocity = new Vector2(player_rb.velocity.x, player_rb.velocity.y * jump_falloff);
-            coyote_time_current = 0f;
-        }
-        */
-        if (hitting_base && hitting_floor && input_move_direction.y > 0f)
+        if ( coyote_time_current > 0f && input_move_direction.y > 0f)
         {
             player_rb.AddForce(Vector2.up * jump_strength_base, ForceMode2D.Impulse);
+            coyote_time_current = 0f;
         }
 
+        if (!(hitting_base && hitting_floor))
+        {
+            if (input_move_direction.y > 0f)
+            {
+                player_rb.velocity = new Vector2(player_rb.velocity.x,
+                    player_rb.velocity.y - gravity_jumping * Time.fixedDeltaTime);
+            }
+            else
+            {
+                player_rb.velocity = new Vector2(player_rb.velocity.x,
+                    player_rb.velocity.y - gravity_base * Time.fixedDeltaTime);
+            }
+        }
+        
         player_rb.velocity = new Vector2(input_move_direction.x * speed_base_floor, player_rb.velocity.y);
     }
 
     private bool ColliderCheck(BoxCollider2D col, LayerMask lm)
     {
         return Physics2D.OverlapBox(col.bounds.center, col.size, 0f, lm);
-    }
-
-    private IEnumerator JumpCooldown()
-    {
-        is_jumping = true;
-        yield return new WaitForSeconds(jump_ienumerator_callback_time);
-        is_jumping = false;
     }
 }
