3ad71dfc7d00fc8384097c4539cf970f18d73cab
Borna Bošnjak <bornabosnjak2006@gmail.com>
2023-02-17 13:57:26 +0100

    nista

--- slimz/Assets/Skripte/PlayerMovement.cs
+++ slimz/Assets/Skripte/PlayerMovement.cs
@@ -9,27 +9,33 @@ public class PlayerMovement : MonoBehaviour
 {
     [Range(0f, 1f)]
     [SerializeField] private float input_activation_strength = 0.2f;
-    [Range(0f, 1f)]
-    [SerializeField] private float input_strength_for_jump = 0.5f;
-    
-    [SerializeField] private float speed_base_floor = 5f;
-    [SerializeField] private float speed_base_air = 5f;
-    [SerializeField] private float jump_base;
+
+    [SerializeField] private float speed_base_floor = 7f;
+    [SerializeField] private float jump_strength_base = 13f;
+    [SerializeField] private float coyote_time = 0.2f;
+    [SerializeField] private float jump_buffer_time = 0.2f;
+    [SerializeField] private float jump_ienumerator_callback_time = 0.4f;
+    [Range(0f,1f)] [Tooltip("MORA BITI ISPOD 1 DA SE SMANJUJE")]
+    [SerializeField] private float jump_falloff = 0.5f;
 
     [Header("Collider References")] 
     [SerializeField] private LayerMask ground_layer;
     [SerializeField] private BoxCollider2D collider_base;
     [SerializeField] private BoxCollider2D collider_floor;
-
+    
+    private Rigidbody2D player_rb;
+    
+    private PlayerInputActions input_map;
+    private InputAction movement;
+    private Vector2 input_move_direction;
+    
     private bool hitting_base;
     private bool hitting_floor;
 
-    private PlayerInputActions input_map;
-    private InputAction movement;
+    private float coyote_time_current;
+    private float jump_buffer_time_current;
+    private bool is_jumping;
 
-    private Vector2 input_direction;
-    
-    private Rigidbody2D player_rb;
 
     private void Awake()
     {
@@ -51,15 +57,15 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         // read the input direction and bind it to 1,1 0,1 1,0 0,0.. format
-        input_direction = movement.ReadValue<Vector2>();
+        input_move_direction = movement.ReadValue<Vector2>();
 
-        if (input_direction.x > input_activation_strength) input_direction.x = 1f;
-        if (input_direction.x < -input_activation_strength) input_direction.x = -1f;
-        if (input_direction.y > input_activation_strength) input_direction.y = 1f;
-        if (input_direction.y < -input_activation_strength) input_direction.y = -1f;
+        if (input_move_direction.x > input_activation_strength) input_move_direction.x = 1f;
+        if (input_move_direction.x < -input_activation_strength) input_move_direction.x = -1f;
+        if (input_move_direction.y > input_activation_strength) input_move_direction.y = 1f;
+        if (input_move_direction.y < -input_activation_strength) input_move_direction.y = -1f;
         
-        if (Mathf.Abs(input_direction.x) < input_activation_strength) input_direction.x = 0f;
-        if (Mathf.Abs(input_direction.y) < input_activation_strength) input_direction.y = 0f;
+        if (Mathf.Abs(input_move_direction.x) < input_activation_strength) input_move_direction.x = 0f;
+        if (Mathf.Abs(input_move_direction.y) < input_activation_strength) input_move_direction.y = 0f;
     }
 
     private void FixedUpdate()
@@ -67,27 +73,56 @@ public class PlayerMovement : MonoBehaviour
         hitting_base = ColliderCheck(collider_base, ground_layer);
         hitting_floor = ColliderCheck(collider_floor, ground_layer);
         
-        // logic while on floor;
+        /*
+        // coyote time and jump buffer
         if (hitting_base && hitting_floor)
         {
-            player_rb.velocity = new Vector2(input_direction.x * speed_base_floor, player_rb.velocity.y);
+            coyote_time_current = coyote_time;
+        }
+        else
+        {
+            if(coyote_time_current > 0f) coyote_time_current -= Time.deltaTime;
         }
         
-        // jump logic
-        if (hitting_base && hitting_floor && input_direction.y > input_strength_for_jump)
+        if (input_move_direction.y > 0f)
         {
-            player_rb.AddForce(Vector2.up * jump_base, ForceMode2D.Impulse);
+            jump_buffer_time_current = jump_buffer_time;
         }
-        
-        // in air logic
-        if (!hitting_floor)
+        else
         {
-            player_rb.velocity = new Vector2(input_direction.x * speed_base_floor, player_rb.velocity.y);
+            if (jump_buffer_time_current > 0f) jump_buffer_time_current -= Time.deltaTime;
         }
+
+        if (coyote_time_current > 0f && jump_buffer_time_current > 0f && !is_jumping)
+        {
+            player_rb.velocity = new Vector2(player_rb.velocity.x, jump_strength_base);
+            jump_buffer_time_current = 0f;
+            StartCoroutine(JumpCooldown());
+        }
+
+        if (input_move_direction.y > 0f && player_rb.velocity.y > 0f)
+        {
+            player_rb.velocity = new Vector2(player_rb.velocity.x, player_rb.velocity.y * jump_falloff);
+            coyote_time_current = 0f;
+        }
+        */
+        if (hitting_base && hitting_floor && input_move_direction.y > 0f)
+        {
+            player_rb.AddForce(Vector2.up * jump_strength_base, ForceMode2D.Impulse);
+        }
+
+        player_rb.velocity = new Vector2(input_move_direction.x * speed_base_floor, player_rb.velocity.y);
     }
 
     private bool ColliderCheck(BoxCollider2D col, LayerMask lm)
     {
         return Physics2D.OverlapBox(col.bounds.center, col.size, 0f, lm);
+    }
+
+    private IEnumerator JumpCooldown()
+    {
+        is_jumping = true;
+        yield return new WaitForSeconds(jump_ienumerator_callback_time);
+        is_jumping = false;
     }
 }
